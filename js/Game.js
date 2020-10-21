525b07c65239fda930fe0a2ff74be788ef5802aa
unknown <kamalas366@gmail.com>
2020-10-21 17:33:26 +0530

    Added the background

--- js/Game.js
+++ js/Game.js
@@ -34,6 +34,11 @@ class Game {
     car3 = createSprite(500,200);
     car4 = createSprite(700,200);
     cars = [car1, car2, car3, car4];
+
+    car1.addImage("car1", car1Img);
+    car2.addImage("car2", car2Img);
+    car3.addImage("car3", car3Img);
+    car4.addImage("car4", car4Img);
   }
 
   play(){
@@ -43,12 +48,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(groundImg);
+      image(track, 0, -displayHeight*4, displayWidth, displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +84,13 @@ class Game {
       player.distance +=10
       player.update();
     }
+    if(player.distance>3860){
+gameState =2;
+    }
 
     drawSprites();
+  }
+  end(){
+    console.log("gameEnded");
   }
 }
